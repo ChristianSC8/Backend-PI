29eaead0e9fc16af258f95a96cb6e6d8ec243d51
ChristianSC8 <christian.supo@upeu.edu.pe>
2023-06-25 23:57:41 -0500

    update category

--- inventario/src/main/java/com/spinmax/inventario/controller/CategoriaController.java
+++ inventario/src/main/java/com/spinmax/inventario/controller/CategoriaController.java
@@ -44,6 +44,6 @@ public class CategoriaController {
     @DeleteMapping("/{id}")
     public String deleteById(@PathVariable(required = true) Integer id) {
         categoriaService.deleteById(id);
-        return "it was deleted correctly";
+        return "";
     }
 }
